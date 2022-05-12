63c57d5698d4128906f44e49c14fd7f8b9a16bdd
Ion Pascari <ionpascari1995@gmail.com>
2022-05-12 15:50:17 +0300

    feat(eureka): add eureka service discovery server

---
+++ eureka-server/src/test/java/inc/dundermifflin/stocks/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.dundermifflin.stocks.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
