5d555ba561cc426115c97b75ea859244911cffe6
Ion Pascari <ionpascari1995@gmail.com>
2022-05-12 17:46:10 +0300

    feat(eureka): register services with eureka

--- licensing-service/src/main/java/inc/dundermifflin/stocks/licensingservice/LicenseServiceApplication.java
+++ licensing-service/src/main/java/inc/dundermifflin/stocks/licensingservice/LicenseServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @RefreshScope //allows a development team to access a /refresh endpoint that will force the Spring Boot application to reread its application configuration.
 @EnableJpaAuditing
+@EnableEurekaClient
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class LicenseServiceApplication {
