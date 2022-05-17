03c715141ea1570cdbeaf7c964accba8f4923094
Ion Pascari <ionpascari1995@gmail.com>
2022-05-17 22:54:05 +0300

    feat(sleuth): fix bug related to tracer

--- gateway-server/src/main/java/inc/dundermifflin/stocks/gatewayserver/filter/CorrelationTrackingPostFilter.java
+++ gateway-server/src/main/java/inc/dundermifflin/stocks/gatewayserver/filter/CorrelationTrackingPostFilter.java
@@ -1,10 +1,13 @@
 package inc.dundermifflin.stocks.gatewayserver.filter;
 
+import brave.Span;
 import brave.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
@@ -14,26 +17,22 @@ import reactor.core.publisher.Mono;
 import java.util.Objects;
 
 @Slf4j
-@Component
+@Configuration
 @RequiredArgsConstructor
-public class CorrelationTrackingPostFilter implements GlobalFilter, Ordered {
+public class CorrelationTrackingPostFilter {
 
     public static final String CORRELATION_ID = "X-Correlation-Id";
-    private final Tracer tracer;
 
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            String traceId = tracer.currentSpan().context().traceIdString();
-            log.info("Injecting correlation id {} into response...", traceId);
-            exchange.getResponse().getHeaders().add(CORRELATION_ID, traceId);
-            log.info("Completing outgoing request for {}.", exchange.getRequest().getURI());
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            Span span = exchange.getAttribute(Span.class.getName());
+            if (span != null) {
+                String traceId = span.context().traceIdString();
+                log.info("Injecting correlation id {} into response...", traceId);
+                exchange.getResponse().getHeaders().add(CORRELATION_ID, traceId);
+                log.info("Completing outgoing request for {}.", exchange.getRequest().getURI());
+            }
         }));
-
-    }
-
-    @Override
-    public int getOrder() {
-        return FilterOrderType.POST.getOrder();
     }
 }
